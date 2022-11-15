3aef4d41de5779087959c5534812bc65bcf1266e
Maiko Soares Gonçalves <64238160+MaikoGoncalves@users.noreply.github.com>
2022-11-15 14:29:58 -0300

    15112022

---
+++ js/authentication.js
@@ -0,0 +1,8 @@
+var btnLogin = document.getElementById('btnLogin');
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+
+btnLogin.addEventListener('click',function(){
+       
+
+});
