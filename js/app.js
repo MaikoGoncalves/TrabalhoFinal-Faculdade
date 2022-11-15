514dcc7ec52df06d206732838a003126a3ef257a
Maiko Soares Gonçalves <64238160+MaikoGoncalves@users.noreply.github.com>
2022-11-15 15:30:08 -0300

    15112022

--- js/app.js
+++ js/app.js
@@ -1,21 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPGHyx4F3nsRe8W_5PGUFcTnJQBQa8q-8",
-  authDomain: "hcjs-c6587.firebaseapp.com",
-  projectId: "hcjs-c6587",
-  storageBucket: "hcjs-c6587.appspot.com",
-  messagingSenderId: "1025547222768",
-  appId: "1:1025547222768:web:c2fe698697562225a60c35",
-  measurementId: "G-R46QXBE5JB"
+  apiKey: "AIzaSyAYx2JPqqZ8nVakVkTCcllzX2VFoK-WTw0",
+  authDomain: "hcjsproj.firebaseapp.com",
+  projectId: "hcjsproj",
+  storageBucket: "hcjsproj.appspot.com",
+  messagingSenderId: "297229943124",
+  appId: "1:297229943124:web:e12f066951204fa36448c1"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
--- js/authentication.js
+++ js/authentication.js
@@ -3,6 +3,15 @@ var email = document.getElementById('email');
 var password = document.getElementById('password');
 
 btnLogin.addEventListener('click',function(){
-       
+    
+firebase.auth().signInWithEmailAndPassword(email.value, password.value).then(function(result) {
+        alert("Usuário Conectado!");
+        window.location.replace('index.html')
+   
+  }).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorMessage);
+  });
 
 });
